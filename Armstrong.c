2d69508164d4eb82574b6601ce1a7a2e7650bb98
vraj-patel4359 <vrajmd4359@gmail.com>
2026-07-02 11:55:38 +0530

    Updating file named Armstrong

--- Armstrong.c
+++ Armstrong.c
@@ -1,39 +1,43 @@
 # include<stdio.h>
 # include<math.h>
 
-int arm(int n);
-int countnum(int n);
+int arm(int c,int n); //Done using recursion
+int countnum(int n); //Done using recursion
 
 int main() {
-        int n;
+        int n,num,flag,sum;
         printf("Enter The Number:");
         scanf("%d",&n);
-        int num
+
+        num=countnum(n);
+        sum=arm(num,n);
+        if(sum==n){
+                printf("The number %d is an Armstrong Number .",n);
+        }
+        else{
+                printf("The number %d is NOT an Armstrong Number .",n);
+        }
         return 0;
 }
 
-int countnum(int n){
+int countnum(int n){  //To count the number of digits
         if(n==0){
                 return 0;
         }
-        int a;
-        a= 1+countnum(n-1);
+        int a= 1+countnum(n/10);
         return a;
 }
 
-int arm(int c,int n){ 
-        if(num==0){
+int arm(int c,int n){  //
+        if(n==0){
                 return 0 ;
         }
         int a,b,r;
         int flag;
-        r=n%10
-        a=arm(n/10)
+        r=n%10;
+        a=arm(c,n/10); //Adds prevision sum to the last remainder
         b=pow(r,c)+a;
-        if(a==n){
-                return flag=1;
-        }
-        else{
-                return flag=2;
+        if(b==n){
+        return b;
         }
 }
