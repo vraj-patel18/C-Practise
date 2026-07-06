189ed453cb7ce06180f98d6551f7d425d218df80
vraj-patel4359 <vrajmd4359@gmail.com>
2026-07-06 22:41:16 +0530

    Updating file named Fibonacci

--- Fibonacci.c
+++ Fibonacci.c
@@ -3,18 +3,21 @@
 void fib(int n);
 
 int main(){
-    fib(10);
+    int n;
+    printf("Enter total number:");
+    scanf("%d",&n);
+    fib(n);
     return 0;
 }
 
 void fib(int n){
-  int a=0,b=1,s,i=2;
-  printf("%d %d ",a,b);
-  while(i<=n){
-    s=a+b;
-    a=b;
-    b=s;
-    printf("%d ",b);
-    i++;
-  }
+  int fib[n];
+  fib[0]=0;
+  fib[1]=1;
+  printf("%d \t %d \t",fib[0],fib[1]);
+
+    for(int i=2;i<n;i++){
+      fib[i]=fib[i-1] + fib[i-2];
+      printf("%d \t"fib[i]);
+    }
 }
